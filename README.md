a8a2514be62d9c5051d382d17fb3b3f0119859b7
Ben Scofield <git@turrean.com>
2014-01-24 09:55:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fairy Tale Remixer
+
+Inspired by [Chuck Wendig's latest Flash Fiction challenge](http://terribleminds.com/ramble/2014/01/24/flash-fiction-challenge-fairy-tales-remixed/). Retell a random fairy tale in a random genre.
---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'bundler'
+Bundler.setup
+
+require 'sinatra'
+require 'json'
+
+genres = JSON.load(File.read('data/genres.json'))
+tales  = JSON.load(File.read('data/fairy-tales.json'))
+
+get '/' do
+  @genre = genres[rand(genres.length)]
+  tale_index = rand(tales.keys.length)
+
+  @tale_title = tales.keys[tale_index]
+  @tale_url   = tales[@tale_title]
+
+  erb :index
+end
